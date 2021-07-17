2109bef6733d2e1f18a3c57cd6926f3d977b2a4a
MatiMorilla <matiasmorilla@hotmail.com>
2021-07-17 13:35:59 -0300

    borra producto anda ma

--- JavaScript/Simulador-Morilla - copia/js/main.js
+++ JavaScript/Simulador-Morilla - copia/js/main.js
@@ -3,9 +3,10 @@ const cart_container = document.querySelector(".cart-container-product");
 const cart_items = document.querySelectorAll(".product");
 const card = document.querySelectorAll(".card");
 const btn_card = document.querySelectorAll(".btn-card");
+const btn_cancel = document.querySelectorAll(".btn-cancel");
 
 
-
+// agrega u producto al carrito
 function addToCart(nombre)
 {
     for(let i = 0; i < arrayP.length; i++)
@@ -23,7 +24,7 @@ function addToCart(nombre)
             <span class="price">$`+ arrayP[i].precio +`</span>
             </div>
             <div class="cancel-container">
-            <button class="btn-cancel"><i class="far fa-times-circle"></i></button>
+            <button class="btn-cancel" idProducto="`+ arrayP[i].id +`"><i class="far fa-times-circle"></i></button>
             </div>`
             
             cart_container.appendChild(product);
@@ -31,18 +32,33 @@ function addToCart(nombre)
     }
 }
 
+function deleteProduct(nombre)
+{
+    let cart_container = document.querySelector(".cart-container-product");
 
+    for(let i = 0; i < arrayP.length; i++)
+    {
+        if(nombre == arrayP[i].nombre)
+        {
+            let cant = cart_container.childNodes;
+            cart_container.removeChild(cant[i + 3]);
+        }
+    }
+}
+
+// oculta o hace aparecer el carrito
 btn_cart.addEventListener("click", function(){
     cart_container.classList.toggle("cart-hidden");
     
 })
 
-
+// Si se hace click en algun boton de "Agregar" se agrega el producto al carrito, se suma el total de la compra y se enumeran los elementos en el carrtio
 btn_card.forEach(function(e){
     e.addEventListener("click", function(){
         let btn_cart = document.querySelector("#btn-cart");
         let cart_items = document.querySelectorAll(".product");
 
+        // AGREGA LOS PRODUCTOS Y SI QUIEREA AGREAGAR MAS DE 7 NO LO DEJA
         if(cart_items.length < 7)
         {
             let p = buscarXid(e.getAttribute("idProducto"));
@@ -53,23 +69,47 @@ btn_card.forEach(function(e){
             alert("La capacidad maxima es de 7, porfavor elimine algunos prodcutos o realice la compra!");
         }
 
-        cart_items = document.querySelectorAll(".product");
-        let price = document.querySelectorAll(".price");
-        let total_price = document.querySelector(".total-price");
 
+        cart_items = document.querySelectorAll(".product");
+        
+        //ENUMERO LOS PRODUCTOS CON EL ATRIBUTO CANT
         if(cart_items.length > 0)
         {
             btn_cart.classList.add("cant");
             btn_cart.setAttribute("cant", cart_items.length);
         }
+        
+        buscarBtnCancel();
 
+        let price = document.querySelectorAll(".price");
+        let total_price = document.querySelector(".total-price");
+        let precioTotal = 0;
+
+        // SUMA EL TOTAL DE TODOS LOS PRODUCTOS
         price.forEach(function(e){
             let precioS = e.innerHTML.slice(1, e.innerHTML.length);
             let precio = parseInt(precioS);
-            let precioTotal = 0;
-            precioTotal += precio; // no suma
-
-            total_price.innerHTML ="Total: $" + precioTotal;
+            precioTotal += precio;
         })
+        total_price.innerHTML ="Total: $" + precioTotal;
     })
 })
+
+
+function buscarBtnCancel()
+{
+    let btn_cancel = document.querySelectorAll(".btn-cancel");
+    let i = 0;
+
+    btn_cancel.forEach(function(e){
+        i++;
+
+        e.addEventListener("click", function(){
+            let p = buscarXid(e.getAttribute("idProducto"));
+            deleteProduct(p.nombre);
+            return 0;
+        })
+    })
+
+}
+
