4ba37748f59f936a9d42090d82c343a53b6fb3b2
MatiMorilla <matiasmorilla@hotmail.com>
2021-07-24 17:52:19 -0300

    Se agrego la clase cart

---
+++ JavaScript/Simulador-Morilla/js/cart.js
@@ -0,0 +1,72 @@
+class Cart
+{
+    constructor()
+    {
+        this.productos = [];
+        this.cantidad = [];
+        this.total = 0;
+    }
+
+    existeProducto(producto)
+    {
+        let pos = -1;
+
+        for(let i = 0; i < this.productos.length; i++)
+        {
+            if(producto.nombre == this.productos[i].nombre)
+            {
+                pos = i;
+            }
+        }
+
+        return pos;
+    }
+
+    agregar(producto)
+    {
+        let pos = this.existeProducto(producto);
+
+        if(pos != -1)
+        {
+            this.cantidad[pos]++;
+        }
+        else
+        {
+            this.productos.push(producto);
+            this.cantidad.push(1);
+        }
+    }
+
+    eliminar(producto)
+    {
+        let pos = this.existeProducto(producto);
+
+        if(pos != -1)
+        {
+            if(this.cantidad[pos] > 1)
+            {
+                this.cantidad[pos]--;
+            }
+            else
+            {
+                this.productos[pos].splice(pos, 1);
+                this.cantidad[pos].splice(pos, 1);
+            }
+        }
+    }
+
+    calcularTotal()
+    {
+        let total = 0;
+
+        if(this.productos.length > 0)
+        {
+            for(let i = 0; i < this.productos.length; i++)
+            {
+                total += this.productos[i].precio * this.cantidad[i];
+            }
+        }
+
+        return total;
+    }
+}
