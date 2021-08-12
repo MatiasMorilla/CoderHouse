063251f48c3fe96f9a8537fa4f612cb6c030ac43
MatiMorilla <matiasmorilla@hotmail.com>
2021-08-12 11:57:37 -0300

    usando jquery

--- Javascript/Clase 5/app.js
+++ Javascript/Clase 5/app.js
@@ -1,9 +1,17 @@
-const prductos = [{nombre: "nombre1", precio: 1500},
-                {nombre: "nombre2", precio: 1600},
-                {nombre: "nombre3", precio: 1700},
-                {nombre: "nombre4", precio: 1800}];
+const productos = [{nombre: "nombre1", precio: 1500},
+            {nombre: "nombre2", precio: 1600},
+            {nombre: "nombre3", precio: 1700},
+            {nombre: "nombre4", precio: 1800},
+            {nombre: "nombre4", precio: 1800},];
 
-for(const producto of productos)
-{
-    
-}
+$(document).ready(function(){
+
+    for(const producto of productos)
+    {
+        $(".contenedor").append(`<div class="producto">
+                            <h2>${producto.nombre}</h2>
+                            <b>${producto.precio}</b>
+                        </div>`);
+    }
+})
+
