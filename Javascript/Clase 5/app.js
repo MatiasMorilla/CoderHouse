5db5e4ef90b779b19868541e5b46e705636af481
MatiMorilla <matiasmorilla@hotmail.com>
2021-08-11 22:27:17 -0300

    jq

---
+++ Javascript/Clase 5/app.js
@@ -0,0 +1,9 @@
+const prductos = [{nombre: "nombre1", precio: 1500},
+                {nombre: "nombre2", precio: 1600},
+                {nombre: "nombre3", precio: 1700},
+                {nombre: "nombre4", precio: 1800}];
+
+for(const producto of productos)
+{
+    
+}
