87c520c911cafc5b74ea8ecb827137f7d83cf93d
MatiMorilla <matiasmorilla@hotmail.com>
2021-08-20 15:19:53 -0300

    agregando validacion y nuevos colores

--- JavaScript/Simulador-Morilla/js/buy.js
+++ JavaScript/Simulador-Morilla/js/buy.js
@@ -18,13 +18,84 @@ $(".btn-buy").click(function()
         });
 
         $(".total-compra").html(`Total: $${cart.calcularTotal()}`);
-
-        $(".input-box input").change(function()
-        {
-            //completar las validaciones
-        });
-
-
     } 
 });
 
+$(function()
+{
+    $("#form-buy").validate({
+        rules: {
+            nombre : {
+              required: true,
+              minlength: 3
+            },
+            apellido: {
+                required: true,
+                minlength: 3
+            },
+            email: {
+              required: true,
+              email: true
+            },
+            direccion: {
+                required: true,
+                minlength: 3
+            },
+            numeroTarjeta: {
+                required: true,
+                minlength: 16,
+                maxlength: 16
+            },
+            nombreTitular: {
+                required: true,
+                minlength: 3
+            },
+            fecha: {
+                required: true,
+            },
+            codigo: {
+                required: true,
+                minlength: 3,
+                maxlength: 3
+            },
+        },
+
+        messages: {
+            nombre : {
+                required: "Porfavor, ingrese su nombre",
+                minlength: "Debe contener minimo 3 caracteres"
+              },
+              apellido: {
+                required: "Porfavor, ingrese su apellido",
+                minlength: "Debe contener minimo 3 caracteres"
+              },
+              email: {
+                required: "Porfavor, ingrese su email",
+                email: "Por favor, introduce una dirección de correo electrónico válida"
+              },
+              direccion: {
+                required: "Porfavor, ingrese su direccion",
+                minlength: "Debe contener minimo 3 caracteres"
+              },
+              numeroTarjeta: {
+                required: "Porfavor, ingrese el numero de su tarjeta",
+                minlength: "Por favor, introduce un numero de tarjeta válida"
+              },
+              nombreTitular: {
+                required: "Porfavor, ingrese su nombre",
+                minlength: "Debe contener minimo 3 caracteres"
+              },
+              fecha: {
+                required: "Porfavor, ingrese la fecha de vencimiento",
+              },
+              codigo: {
+                required: "Porfavor, ingrese el CVV de su tarjeta",
+                minlength: "Por favor, introduce un CVV válido"
+              },
+
+              submitHandler: function(form) {
+                form.submit();
+              }
+        }
+    });
+});
